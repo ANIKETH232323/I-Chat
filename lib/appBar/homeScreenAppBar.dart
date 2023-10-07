1345214e1473a4392d84e9f6992a0f916d22f695
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-07 12:10:07 +0530

    Done

--- lib/appBar/homeScreenAppBar.dart
+++ lib/appBar/homeScreenAppBar.dart
@@ -1,95 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:i_chat/widgets/chat_user_card.dart';
 
-class homeScreenAppBar extends StatelessWidget {
-  const homeScreenAppBar({super.key});
+class Custom_Navigation_Button extends StatelessWidget {
+  final Widget icon;
+  final Function()? onPressed;
+  const Custom_Navigation_Button({
+    super.key,
+    required this.icon,
+    this.onPressed
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.square(185),
-        child: AppBar(
-          toolbarHeight: 80,
-          title: Text("I Chat"),
-          centerTitle: true,
-          leading: IconButton(
-              onPressed: () {},
-              icon: Image.asset(
-                'images/acc.png',
-                width: 85,
-                height: 85,
-              ),
-              padding: EdgeInsets.only(left: 15)
-              // EdgeInsets.symmetric(horizontal: mq.width * .02, vertical: 10),
-              ),
-          actions: [
-            // 3dot button
-            IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
-
-
-          ],
-          backgroundColor: Color.fromARGB(255, 88, 45, 210),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25)),
-          ),
-        ),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25)
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: Column(
-
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.deepOrange,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25)
-                        )
-                    ) ,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              child: Text("Messages",
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 88, 45, 210),
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold)),
-              padding: EdgeInsets.only(left: 30, bottom: 8),
-            ),
-            Flexible(
-              child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 15,
-                  itemBuilder: (context, index) {
-                    return chat_user_card();
-                  }),
-            ),
-          ],
-        ),
-      ),
+      child: IconButton(onPressed: onPressed,icon: icon,),
     );
-
   }
 }
 
-// TextField(
-// decoration: InputDecoration(
-// filled: true,
-// fillColor: Colors.deepOrange,
-// border: OutlineInputBorder(
-// borderRadius: BorderRadius.circular(25)
-// )
-// ) ,
-// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:i_chat/appBar/homeScreenAppBar.dart';
+import 'package:i_chat/main.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -12,7 +13,103 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return homeScreenAppBar();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 88, 45, 210),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35))),
+              height: double.infinity,
+              margin: EdgeInsets.only(top: mq.height * .25),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: Column(
+                children: [
+                  _HeaderSection(),
+                  searchBox(),
+                ],
+              ),
+            )
+          ],
+        ),
+        bottom: false,
+      ),
+    );
   }
 }
 
+class searchBox extends StatelessWidget {
+  const searchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: Colors.white60,
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(color: Colors.white)
+
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset('images/search.png')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class _HeaderSection extends StatelessWidget {
+  const _HeaderSection({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage('images/acc.png'),
+            ),
+            Custom_Navigation_Button(
+              icon: Icon(Icons.menu_rounded),
+            )
+          ],
+        ),
+        // Padding(
+        //   padding: const EdgeInsets.only(top: 120.0),
+        //   child: SizedBox(
+        //     width: 360,
+        //     height: 40,
+        //     child: TextField(
+        //       decoration: InputDecoration(
+        //           filled: true,
+        //           contentPadding: EdgeInsets.symmetric(horizontal: 18),
+        //           hintText: 'Search Messages',
+        //           hintStyle: TextStyle(color: Colors.white,),
+        //           fillColor: Colors.white30,
+        //           border: OutlineInputBorder(
+        //               borderRadius: BorderRadius.circular(25))),
+        //     ),
+        //   ),
+        // ),
+
+      ],
+    );
+  }
+}
