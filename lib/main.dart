9ec29961018bfae38757a19ff53d8cea21f07000
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-09 01:09:35 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:i_chat/screens/auth/login_screen.dart';
 import 'firebase_options.dart';
 
 
 late Size mq;
-void main()  {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]).then((value){
