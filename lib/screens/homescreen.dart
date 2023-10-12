abbd73841bcca167be4df499ecf95322c734c991
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-13 02:41:08 +0530

    Done

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -10,18 +10,12 @@ import 'package:i_chat/widgets/chat_user_card.dart';
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
-
-
-
-
-
   @override
   State<Homescreen> createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
   List<ChatUser> list = [];
-
 
   @override
   void initState() {
@@ -41,12 +35,17 @@ class _HomescreenState extends State<Homescreen> {
               children: [
                 Container(
                   alignment: Alignment.bottomLeft,
-                  padding: EdgeInsets.only(left: mq.height * .045,top: mq.height * .045),
-                  child: IconButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (_)=>ProfileScreen(user: ApIs.me)));
-                  },
-                    icon: new Image.asset("images/acc.png"),),
+                  padding: EdgeInsets.only(
+                      left: mq.height * .045, top: mq.height * .045),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => ProfileScreen(user: ApIs.me)));
+                    },
+                    icon: new Image.asset("images/acc.png"),
+                  ),
                 ),
 
                 // CachedNetworkImage(
@@ -57,52 +56,60 @@ class _HomescreenState extends State<Homescreen> {
                 //   // placeholder: (context, url) => CircularProgressIndicator(),
                 //   errorWidget: (context, url, error) => CircleAvatar(child: Icon(CupertinoIcons.person),backgroundColor: Colors.amberAccent),
                 // ),
-                SizedBox(height: mq.height * .015,),
+                SizedBox(
+                  height: mq.height * .015,
+                ),
                 searchBox(),
                 Flexible(
                   fit: FlexFit.tight,
                   child: Container(
-                    decoration: BoxDecoration(color: Colors.white,
-                      borderRadius: BorderRadius.only(topRight: Radius.circular(25),topLeft: Radius.circular(25))
-                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(25),
+                            topLeft: Radius.circular(25))),
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: StreamBuilder(
                         stream: ApIs.getAllUser(),
                         builder: (context, snapshot) {
                           switch (snapshot.connectionState) {
-                          // if Data is loading
+                            // if Data is loading
                             case ConnectionState.waiting:
                             case ConnectionState.none:
-                              return const Center(child: CircularProgressIndicator());
-                          // if some or all data is loaded then show it
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            // if some or all data is loaded then show it
                             case ConnectionState.active:
                             case ConnectionState.done:
                               final data = snapshot.data?.docs;
                               list = data
-                                  ?.map((e) => ChatUser.fromJson(e.data()))
-                                  .toList() ??
+                                      ?.map((e) => ChatUser.fromJson(e.data()))
+                                      .toList() ??
                                   [];
                           }
-                          if(list.isNotEmpty){
+                          if (list.isNotEmpty) {
                             return ListView.builder(
                                 physics: BouncingScrollPhysics(),
                                 itemCount: list.length,
                                 itemBuilder: (context, index) {
-                                  return chat_user_card(user: list[index],);
+                                  return chat_user_card(
+                                    user: list[index],
+                                  );
                                   // return Text('Name:${list[index]}');
                                 });
-                          }
-                          else{
-                            return Center(child: Text('No Connection Found',style: TextStyle(fontSize: 18),));
+                          } else {
+                            return Center(
+                                child: Text(
+                              'No Connection Found',
+                              style: TextStyle(fontSize: 18),
+                            ));
                           }
                         },
                       ),
                     ),
                   ),
                 )
-
-
               ],
             )
             // Padding(
@@ -114,10 +121,6 @@ class _HomescreenState extends State<Homescreen> {
             //     ],
             //   ),
             // ),
-
-
-
-
           ],
         ),
       ),
@@ -132,11 +135,10 @@ class searchBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Padding(
-      padding: EdgeInsets.all( mq.height * .019),
+      padding: EdgeInsets.all(mq.height * .019),
       child: Container(
-        height: mq.height *.05,
+        height: mq.height * .05,
         padding: EdgeInsets.symmetric(horizontal: 15),
         decoration: BoxDecoration(
           color: Colors.white60,
@@ -148,7 +150,7 @@ class searchBox extends StatelessWidget {
               children: [
                 Flexible(
                     child: SizedBox(
-                  height: mq.height *.05,
+                  height: mq.height * .05,
                   child: TextField(
                     decoration: InputDecoration(
                       contentPadding: EdgeInsets.only(bottom: 5),
@@ -169,27 +171,3 @@ class searchBox extends StatelessWidget {
   }
 }
 
-class _HeaderSection extends StatelessWidget {
-  const _HeaderSection();
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (_)=>ProfileScreen(user: ApIs.me)));
-              },
-              icon: new Image.asset("images/acc.png"),)
-            // Custom_Navigation_Button(
-            //   icon: Icon(Icons.menu_rounded),
-            // )
-          ],
-        ),
-      ],
-    );
-  }
-}
