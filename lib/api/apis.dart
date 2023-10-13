118f26b0368225ce83870c142c1317c257969940
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-13 14:43:27 +0530

    Done

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:i_chat/models/chatUse.dart';
 
 class ApIs{
   static FirebaseAuth auth = FirebaseAuth.instance;
+  static FirebaseStorage storage = FirebaseStorage.instance;
 
   // for storing self info
   static late ChatUser me;
