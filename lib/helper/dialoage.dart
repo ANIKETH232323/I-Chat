4559d456a303b0789954033823a5344d2c6c52d1
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-14 11:11:51 +0530

    Done

--- lib/helper/dialoage.dart
+++ lib/helper/dialoage.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:i_chat/main.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class Dialogs{
   static void showSnackBar(BuildContext context,String msg){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg),
-    backgroundColor: Colors.black,
-    behavior: SnackBarBehavior.floating,));
+    showTopSnackBar(
+      Overlay.of(context),
+
+      CustomSnackBar.error(
+          messagePadding: EdgeInsets.only(left: mq.height *.04),
+          message:msg),
+    );
   }
 
 
