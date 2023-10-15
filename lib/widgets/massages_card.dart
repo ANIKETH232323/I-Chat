ef26285afb6fda47f3ef5a273bf6861b726ab69d
Aniketh Pratihar <aniketh5660@gmail.com>
2023-10-15 20:48:18 +0530

    Done

--- lib/widgets/massages_card.dart
+++ lib/widgets/massages_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:i_chat/api/apis.dart';
+import 'package:i_chat/main.dart';
 import 'package:i_chat/models/Message.dart';
 
 class MessageCard extends StatefulWidget{
@@ -33,16 +34,16 @@ class _MessageCardState extends State<MessageCard> {
           child: Flexible(
             child: Container(
               padding: EdgeInsets.all(15),
-              margin: EdgeInsets.only(top: 30,left: 25),
+              margin: EdgeInsets.only(top: mq.height * .01,left: mq.width * .05),
               decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 151, 71, 255),
+                  color: Color.fromARGB(255, 197, 197, 195),
                   borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30),bottomRight: Radius.circular(30),)
               ),
               child: Text(widget.message.msg+" How are You doing?",
 
-                style:TextStyle(color: Colors.white,
+                style:TextStyle(color: Color.fromARGB(255, 67, 56, 56),
                   fontSize: 15,
-                  fontWeight: FontWeight.w500,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
@@ -51,10 +52,10 @@ class _MessageCardState extends State<MessageCard> {
         // For Time and Double right Button
         Row(children: [
           Container(
-              margin: EdgeInsets.only(top: 5,left: 30),
+              margin: EdgeInsets.only(top:mq.height * .008,left: mq.height * .03),
               child: Icon(Icons.done_all_rounded,color: Colors.blue)),
           Container(
-              margin: EdgeInsets.only(left:5,right: 10,top: 5),
+              margin: EdgeInsets.only(left:mq.width * .02,top: mq.height * .008),
               child: Text("12.00 AM",
                 style: TextStyle(fontSize: 12,fontWeight: FontWeight.w600),
               )),
@@ -74,7 +75,7 @@ class _MessageCardState extends State<MessageCard> {
           child: Flexible(
             child: Container(
               padding: EdgeInsets.all(15),
-              margin: EdgeInsets.only(top: 30,left: 25),
+              margin: EdgeInsets.only(top: mq.height * .04),
               decoration: BoxDecoration(
                   color: Color.fromARGB(255, 151, 71, 255),
                   borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30),bottomLeft: Radius.circular(30),)
@@ -82,7 +83,7 @@ class _MessageCardState extends State<MessageCard> {
               child: Text(widget.message.msg,
                 style:TextStyle(color: Colors.white,
                   fontSize: 15,
-                  fontWeight: FontWeight.w500,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
@@ -95,10 +96,10 @@ class _MessageCardState extends State<MessageCard> {
           children: [
             Container(
 
-                margin: EdgeInsets.only(top: 1),
+                margin: EdgeInsets.only(top: mq.height * .005),
                 child: Icon(Icons.done_all_rounded,color: Colors.blue)),
           Container(
-              margin: EdgeInsets.only(left: 5,right: 10,top: 5),
+              margin: EdgeInsets.only(left: mq.width * .02,right: mq.width * .065,top: mq.height * .005),
               child: Text("12.00 AM",
                 style: TextStyle(fontSize: 12,fontWeight: FontWeight.w600),
               )),
